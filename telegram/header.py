0311c0c7d017903208b45382b04edc93236fe907
TerliPower <116505407+TerliPower@users.noreply.github.com>
2023-04-05 16:38:14 +0500

    Add files via upload

---
+++ telegram/header.py
@@ -0,0 +1,12 @@
+from os.path import exists
+from glob import glob
+import re
+import random
+import datetime
+import os
+import time
+from random import randint
+import telebot
+from telebot import types
+#bot = telebot.TeleBot('1877401711:AAF8qEYooYn-gzuYxktLiRQkMQ8jnqoWCCQ')
+bot = telebot.TeleBot('5898826480:AAE_HCmCajhiVtdB1Q-Kxu-gRpaEm0qhOOY')
---
+++ telegram/text.py
@@ -0,0 +1,3 @@
+def TransmitContracts(sender, receiver, contractname, amount):
+    with open(f'curr/{contractname', 'r') as f:
+        ZZ
